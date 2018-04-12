4adf3b53edf9a86f48189684ee1c855ab2ab23f6
ngou-long.kam <ngou-long.kam@digital.hmrc.gov.uk>
2018-04-12 17:47:08 +0100

    Changed package name to use g8 tag

--- src/main/g8/src/test/scala/uk.gov.hmrc.$packageName$/tags/RunIn.scala
+++ src/main/g8/src/test/scala/uk.gov.hmrc.$packageName$/tags/RunIn.scala
@@ -2,6 +2,6 @@ package uk.gov.hmrc.$packageName$.tags
 
 import org.scalatest.Tag
 
-object RunOnlyInQA extends Tag("uk.gov.hmrc.selfservicetimetopay.tags.RunOnlyInQA")
-object RunOnlyInDev extends Tag("uk.gov.hmrc.selfservicetimetopay.tags.RunOnlyInDev")
+object RunOnlyInQA extends Tag(s"uk.gov.hmrc.$packageName$.tags.RunOnlyInQA")
+object RunOnlyInDev extends Tag(s"uk.gov.hmrc.$packageName$.tags.RunOnlyInDev")
 
